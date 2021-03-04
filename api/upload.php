5aaeb82b42d83cddbb39c53e8ec96c90ef7e51b1
Kevin <ssemkeltyl@gmail.com>
2021-03-04 19:10:39 +0300

    c

--- api/upload.php
+++ api/upload.php
@@ -19,7 +19,11 @@ if($_FILES['file']){
 } */
 
 header('Access-Control-Allow-Origin: *');
-$target_path = "uploads/";
+
+
+
+
+$target_path = "upload/";
 
 $target_path = $target_path.basename ($_FILES['file']['name']);
 

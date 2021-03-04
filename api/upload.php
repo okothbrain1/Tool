3cceb2f1be024ec65c4dd557b83baba01a34e8d8
Kevin <ssemkeltyl@gmail.com>
2021-03-04 09:44:00 +0300

    c

--- api/upload.php
+++ api/upload.php
@@ -1,13 +1,20 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-$target_path = "upload/";
- 
-$target_path = $target_path . basename( $_FILES['file']['name']);
- 
-if (move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
-    echo "Upload and move success";
-} else {
-echo $target_path;
-    echo "There was an error uploading the file, please try again!";
+$name = $_POST['name'];
+if($_FILES['file']){
+    $path = 'uploads/';
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
+    $originalName = $_FILES['file']['name'];
+    $ext = '.'.pathinfo($originalName, PATHINFO_EXTENSION);
+    $t=time();
+    $generatedName = md5($t.$originalName).$ext;
+    $filePath = $path.$generatedName;
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $filePath)) {
+        echo json_encode(array(
+            'result' => 'success',
+'status' => true,
+));
+}
 }
 ?>
